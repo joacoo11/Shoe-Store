53230fd031a7e15682f4f8cb03885a695d3d0345
joacoo11 <joacolobrutto11@gmail.com>
2022-04-25 18:22:40 -0300

    Second commit

---
+++ js/shoes.js
@@ -0,0 +1,15 @@
+// const shoes = [{shoe: "Kyrie 4", precio: 32000, img: "../Shoes/4.png"},
+// {shoe:"Kyrie 3", precio: 35000, img:"../Shoes/ky3.png"},
+// {shoe:"LeBron 12", precio: 41000, img: "../Shoes/lebro.png"}]
+
+// for(let i=0; i<shoes.length; i+=1){
+// stock.innerHTML +=` 
+// <div class="card" style="width: 18rem;">
+//   <img src="${shoes[i].img}" class="card-img-top" alt="Kyrie 4">
+//   <div class="card-body">
+//     <h5 class="card-title">${shoes[i].shoe}</h5>
+//     <p class="card-text">${shoes[i].precio}</p>
+//     <a href="" class="btn btn-primary">Buy</a>
+//   </div>
+// </div>
+`}
